2142e06b937a801ac282cfa3f42d34ef0cccdb49
Ulisses <ulisses.cruz86@hotmail.com>
2023-10-10 13:34:56 -0300

    Primeira versão

---
+++ QueHoraVouSair.bat
@@ -0,0 +1,1 @@
+java --module-path "C:\Program Files\Java\openjfx-21_windows-x64_bin-sdk\javafx-sdk-21\lib" --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.web --add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED -jar .\out\artifacts\QueHoraVouSair_jar\QueHoraVouSair.jar
