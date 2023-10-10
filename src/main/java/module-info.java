020205737d5f09a0477df4eb8b99a8d058bde9bb
Ulisses <ulisses.cruz86@hotmail.com>
2023-10-10 13:34:06 -0300

    Primeira versão

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.quehoravousair {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.quehoravousair to javafx.fxml;
-    exports com.example.quehoravousair;
-}
