207a7c2f4722d84a07ac5e5cde2a83124d874ff1
UlissesCruz86 <ulisses.cruz86@hotmail.com>
2024-08-02 21:04:14 -0300

    Atualizando a nova versão

--- QueHoraVouSair.bat
+++
@@ -1,3 +0,0 @@
-java --module-path "C:\Program Files\Java\jdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.web --add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED -jar .\out\artifacts\QueHoraVouSair_jar\QueHoraVouSair.jar
-
-pause
